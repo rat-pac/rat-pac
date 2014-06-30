rat-pac/rat-pac

commit ea080d12b28b5f4b8113d7aaf2cb6017134badbd

add r11780 pmt, load surfaces from db, various fixes
* Modify "sphere" geometry to match "challenge" requirements
* Add model for Hamamatsu R11780 HQE PMT
* Load optical surfaces dynamically from DB rather than hard-coding in
  Materials.cc
* Fix a failure to sample the PMT charge and write the front-end time,
  which was an oversight while sanitizing RAT for public use.

src/core/Gsim.cc
@@ -28,7 +28,10 @@
 #include <RAT/GLG4DebugMessenger.hh>
 #include <RAT/GLG4VertexGen.hh>
 
+#include <RAT/PMTTime.hh>
+#include <RAT/PMTCharge.hh>
 #include <RAT/TimeUtil.hh>
+#include <RAT/PMTTime.hh>
 #include <RAT/Config.hh>
 
 #include <Randomize.hh>
@@ -121,6 +124,10 @@ void Gsim::Init() {
   // ...and finally the hook.  Here's where we trap GEANT4 at the end of
   // each event and do our business.
   theRunManager->SetUserAction(static_cast<G4UserEventAction*>(this));
+
+  // PMT transit time and single-pe charge calculators
+  fPMTTime = NULL;
+  fPMTCharge = NULL;
 }
 
 Gsim::~Gsim() {
@@ -130,6 +137,9 @@ Gsim::~Gsim() {
   // SetUserAction() is overloaded.
   theRunManager->SetUserAction(static_cast<G4UserEventAction*>(NULL));
   theRunManager->SetUserAction(static_cast<G4UserTrackingAction*>(NULL));
+
+  delete fPMTTime;
+  delete fPMTCharge;
 }
   
 void Gsim::BeginOfRunAction(const G4Run* /*aRun*/) {
@@ -142,6 +152,12 @@ void Gsim::BeginOfRunAction(const G4Run* /*aRun*/) {
 
   DBLinkPtr ldaq = DB::Get()->GetLink("DAQ");
   channelEfficiency = ldaq->GetD("channel_efficiency");
+
+  delete fPMTTime;
+  fPMTTime = new RAT::PMTTime();
+
+  delete fPMTCharge;
+  fPMTCharge = new RAT::PMTCharge();
 
   // Tell the generator when the run starts
   GLG4PrimaryGeneratorAction* theGLG4PGA= 
@@ -457,8 +473,8 @@ void Gsim::MakeEvent(const G4Event* g4ev, DS::Root* ds) {
   GLG4HitPMTCollection* hitpmts = GLG4VEventAction::GetTheHitPMTCollection();
   int numPE = 0;
  
-  double firsthittime = 99999;
-  double lasthittime = -99999;
+  double firsthittime = DBL_MAX;
+  double lasthittime = -DBL_MAX;
 
   // Get the PMT type for IDPMTs. Then in the loop,
   // increment numPE only when the PE is in an IDPMT.
@@ -563,6 +579,8 @@ void Gsim::AddMCPhoton(DS::MCPMT* rat_mcpmt, const GLG4HitPhoton* photon,
     rat_mcphoton->SetTrackID(-1);
   }
   rat_mcphoton->SetHitTime(photon->GetTime());
+  rat_mcphoton->SetFrontEndTime(fPMTTime->PickTime(photon->GetTime()));
+  rat_mcphoton->SetCharge(fPMTCharge->PickCharge());
 }
 
 void Gsim::SetStoreParticleTraj(const G4String& particleName,

data/MATERIALS.ratdb
@@ -312,6 +312,18 @@ elemprop: [0.4675d, 0.5325d],
 
 {
 name: "MATERIAL",
+index: "mirror",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+density: 2.2d,
+nelements: 2,
+nmaterials: 0,
+elements: ["Silicon", "Oxygen",],
+elemprop: [0.4675d, 0.5325d],
+}
+
+{
+name: "MATERIAL",
 index: "photocathode",
 valid_begin : [0, 0],
 valid_end : [0, 0],
@@ -352,6 +364,18 @@ elemprop: [1d],
 {
 name: "MATERIAL",
 index: "photocathode_R1408",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+density: 5.d,
+nelements: 1,
+nmaterials: 0,
+elements: ["Potassium"],
+elemprop: [1d],
+}
+
+{
+name: "MATERIAL",
+index: "photocathode_R11780",
 valid_begin : [0, 0],
 valid_end : [0, 0],
 density: 5.d,
@@ -403,18 +427,16 @@ elemprop: [0.38889d, 0.55873d, 0.05238d,],
 }
 
 //// hamamatsu borosilicate glass composition
-//// Sum of element composition = 1.001, needs to be checked
-//// Comment:  JWALDING 20121019
 {
 name: "MATERIAL",
 index: "hamamatsu_borosilicate_glass",
 valid_begin : [0, 0],
 valid_end : [0, 0],
-density: 2.23d, // g/cm^3, from Wikipedia, same as borosilicate glass above, probably needs checking - JWALDING 20121019
+density: 2.23d, // g/cm^3, from Wikipedia
 nelements: 7,
 nmaterials: 0,
 elements: ["Lithium","Boron","Oxygen","Sodium","Aluminum","Silicon","Barium",],
-elemprop: [0.001d, 0.059d, 0.523d, 0.042d, 0.049d, 0.285d, 0.042d,],
+elemprop: [0.001d, 0.059d, 0.523d, 0.042d, 0.049d, 0.284d, 0.042d,],
 }
 
 {
@@ -679,30 +701,6 @@ nelements: 2,
 nmaterials: 0,
 elements: ["Carbon","Hydrogen",],
 elemprop: [0.8563d, 0.1437d],
-}
-
-{
-name: "MATERIAL",
-index: "tank_liner",
-valid_begin : [0, 0],
-valid_end : [0, 0],
-density: 96.0d,
-nelements: 2,
-nmaterials: 0,
-elements: ["Carbon","Hydrogen",],
-elemprop: [0.8563d, 0.1437d],
-}
-
-{
-name: "MATERIAL",
-index: "polyethylene",
-valid_begin : [0, 0],
-valid_end : [0, 0],
-density: 0.91d,
-nelements: 2,
-nmaterials: 0,
-elements: ["Hydrogen", "Carbon"],
-elemprop: [0.1437d, .8563d],
 }
 
 {

data/OPTICS.ratdb
@@ -79,6 +79,11 @@ name: "OPTICS",
 index: "stainless_steel",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.1,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d,   200d,  300d,  400d,   600d,  800d,  ],
 REFLECTIVITY_value2: [0.35d, 0.35d, 0.45d, 0.55d,  0.65d, 0.67d, ],
@@ -96,6 +101,11 @@ name: "OPTICS",
 index: "stainless_steel_316L",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.1,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d, 120d, 140d,   200d,  300d,  400d,   600d,  800d,  ],
 REFLECTIVITY_value2: [0.0d, 0.0d, 0.35d, 0.35d, 0.45d, 0.55d,  0.65d, 0.67d, ],
@@ -110,6 +120,11 @@ name: "OPTICS",
 index: "aluminum",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.9,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [ 60.d,317.d,317.56338d, 323.98899d, 330.41461d, 334.25796d, 344.31949d, 351.2497d, 358.22436d, 368.79049d, 379.34921d, 388.86909d, 402.49245d, 416.1158d, 429.70211d, 445.35868d, 460.9708d, 477.08011d, 492.14318d, 508.24508d, 522.78873d, 540.44147d, 555.48972d, 569.50654d, 585.06681d, 601.13908d, 615.10404d, 631.15408d, 645.63846d, 660.64968d, 676.69231d, 692.23775d, 706.72954d, 721.73334d, 737.24915d, 751.20671d, 766.7077d, 781.16986d, 795.60239d, 808.48407d, ],
 REFLECTIVITY_value2: [  1.0d-3, 1.0d-3, 75.91648d-2,  77.98801d-2,  80.05955d-2,  82.28502d-2,  84.36296d-2,  86.2769d-2,  88.66637d-2,  90.58671d-2,  92.42781d-2,  94.26708d-2,  95.5589d-2,  96.8507d-2,  97.74627d-2,  98.56623d-2,  98.91068d-2,  99.01829d-2,  99.04481d-2,  99.07316d-2,  99.09877d-2,  99.05061d-2,  98.91864d-2,  98.86408d-2,  98.65376d-2,  98.36511d-2,  97.75578d-2,  97.22937d-2,  96.62096d-2,  96.09272d-2,  95.48705d-2,  95.11823d-2,  94.58907d-2,  93.98158d-2,  93.29575d-2,  92.60717d-2,  91.76283d-2,  90.91666d-2,  89.75349d-2,  88.66682d-2, ],
@@ -138,6 +153,11 @@ name: "OPTICS",
 index: "ptfe",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.01,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d, 100d, 250d, 400d, 15000d, 2200d, ],
 REFLECTIVITY_value2: [0.0d, 0.0d, 0.9d, 0.9d, 0.9d, 0.9d, ],
@@ -152,6 +172,11 @@ name: "OPTICS",
 index: "ptfe_fabric", // Gore DRP Diffuse Reflector (ePTFE)
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.01,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d, 100d, 200d, 400d, 15000d, 2200d, ],  // Gore reflectance data ends at 400nm
 REFLECTIVITY_value2: [0.0d, 0.0d, 0.95d, 0.993d, 0.993d, 0.993d, ],
@@ -166,6 +191,11 @@ name: "OPTICS",
 index: "ZnO2",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.01,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d, 100d, 250d, 400d, 15000d, 2200d, ],
 REFLECTIVITY_value2: [0.0d, 0.0d, 0.95d, 0.97d, 0.97d, 0.95d, ],
@@ -238,6 +268,11 @@ name: "OPTICS",
 index: "sapphire",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_dielectric",
+polish: 0.9,
 RINDEX_option: "wavelength",
 RINDEX_value1: [60d, 200d, 300d, 600d, 800d, ],
 RINDEX_value2: [1.76d, 1.76d, 1.76d, 1.76d, 1.76d, ], //index of refraction looked up online
@@ -256,6 +291,11 @@ name: "OPTICS",
 index: "quartz",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_dielectric",
+polish: 0.9,
 RINDEX_option: "wavelength",
 RINDEX_value1: [60d, 200d, 300d, 600d, 800d, ],
 RINDEX_value2: [1.458d, 1.458d, 1.458d, 1.458d, 1.458d, ],
@@ -270,6 +310,11 @@ name: "OPTICS",
 index: "glass",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_dielectric",
+polish: 0.9,
 RINDEX_option: "wavelength",
 RINDEX_value1: [60d, 200d, 300d, 600d, 800d, ],
 RINDEX_value2: [1.458d, 1.458d, 1.458d, 1.458d, 1.458d, ],
@@ -279,6 +324,21 @@ ABSLENGTH_value2: [0.1d-3, 0.1d-3, 0.1d-3, 1.0d3, 2.0d3, 1.0d3, 0.5d3, 0.1d-3, ]
 PROPERTY_LIST: ["RINDEX", "ABSLENGTH", ]
 }
 
+{
+name: "OPTICS",
+index: "mirror",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+surface: 1,
+finish: "polishedfrontpainted",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.999,
+REFLECTIVITY_option: "wavelength",
+REFLECTIVITY_value1: [60d, 800d, ],
+REFLECTIVITY_value2: [0.9999d, 0.9999d, ],
+PROPERTY_LIST: ["REFLECTIVITY", ]
+}
 
 ////   Below is the standard R5912 photocathode
 {
@@ -286,6 +346,7 @@ name: "OPTICS",
 index: "photocathode",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+photocathode: 1,
 
 EFFICIENCY_option: "wavelength",
 EFFICIENCY_value1: [60d, 200d, 260d, 270d, 280d, 285d, 290d, 300d, 310d, 330d, 370d, 420d, 475d, 500d, 530d, 570d, 600d, 630d, 670d, 700d, 800d, ],
@@ -310,6 +371,7 @@ name: "OPTICS",
 index: "photocathode_R11065",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+photocathode: 1,
 
 EFFICIENCY_option: "wavelength",
 EFFICIENCY_value1: [60d, 200d, 260d, 270d, 280d, 285d, 290d, 300d, 310d, 330d, 370d, 420d, 475d, 500d, 530d, 570d, 600d, 630d, 670d, 700d, 800d, ],
@@ -334,6 +396,7 @@ name: "OPTICS",
 index: "photocathode_R1408",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+photocathode: 1,
 
 EFFICIENCY_option: "wavelength",
 EFFICIENCY_value1: [ 60d, 200d, 230d, 231d, 232d, 233d, 234d, 235d, 236d, 237d, 238d, 239d, 240d, 241d, 242d, 243d, 244d, 245d, 246d, 247d, 248d, 249d, 250d, 251d, 252d, 253d, 254d, 255d, 256d, 257d, 258d, 259d, 260d, 261d, 262d, 263d, 264d, 265d, 266d, 267d, 268d, 269d, 270d, 271d, 272d, 273d, 274d, 275d, 276d, 277d, 278d, 279d, 280d, 281d, 282d, 283d, 284d, 285d, 286d, 287d, 288d, 289d, 290d, 291d, 292d, 293d, 294d, 295d, 296d, 297d, 298d, 299d, 300d, 301d, 302d, 303d, 304d, 305d, 306d, 307d, 308d, 309d, 310d, 311d, 312d, 313d, 314d, 315d, 316d, 317d, 318d, 319d, 320d, 321d, 322d, 323d, 324d, 325d, 326d, 327d, 328d, 329d, 330d, 331d, 332d, 333d, 334d, 335d, 336d, 337d, 338d, 339d, 340d, 341d, 342d, 343d, 344d, 345d, 346d, 347d, 348d, 349d, 350d, 351d, 352d, 353d, 354d, 355d, 356d, 357d, 358d, 359d, 360d, 361d, 362d, 363d, 364d, 365d, 366d, 367d, 368d, 369d, 370d, 371d, 372d, 373d, 374d, 375d, 376d, 377d, 378d, 379d, 380d, 381d, 382d, 383d, 384d, 385d, 386d, 387d, 388d, 389d, 390d, 391d, 392d, 393d, 394d, 395d, 396d, 397d, 398d, 399d, 400d, 401d, 402d, 403d, 404d, 405d, 406d, 407d, 408d, 409d, 410d, 411d, 412d, 413d, 414d, 415d, 416d, 417d, 418d, 419d, 420d, 421d, 422d, 423d, 424d, 425d, 426d, 427d, 428d, 429d, 430d, 431d, 432d, 433d, 434d, 435d, 436d, 437d, 438d, 439d, 440d, 441d, 442d, 443d, 444d, 445d, 446d, 447d, 448d, 449d, 450d, 451d, 452d, 453d, 454d, 455d, 456d, 457d, 458d, 459d, 460d, 461d, 462d, 463d, 464d, 465d, 466d, 467d, 468d, 469d, 470d, 471d, 472d, 473d, 474d, 475d, 476d, 477d, 478d, 479d, 480d, 481d, 482d, 483d, 484d, 485d, 486d, 487d, 488d, 489d, 490d, 491d, 492d, 493d, 494d, 495d, 496d, 497d, 498d, 499d, 500d, 501d, 502d, 503d, 504d, 505d, 506d, 507d, 508d, 509d, 510d, 511d, 512d, 513d, 514d, 515d, 516d, 517d, 518d, 519d, 520d, 521d, 522d, 523d, 524d, 525d, 526d, 527d, 528d, 529d, 530d, 531d, 532d, 533d, 534d, 535d, 536d, 537d, 538d, 539d, 540d, 541d, 542d, 543d, 544d, 545d, 546d, 547d, 548d, 549d, 550d, 551d, 552d, 553d, 554d, 555d, 556d, 557d, 558d, 559d, 560d, 561d, 562d, 563d, 564d, 565d, 566d, 567d, 568d, 569d, 570d, 571d, 572d, 573d, 574d, 575d, 576d, 577d, 578d, 579d, 580d, 581d, 582d, 583d, 584d, 585d, 586d, 587d, 588d, 589d, 590d, 591d, 592d, 593d, 594d, 595d, 596d, 597d, 598d, 599d, 600d, 601d, 602d, 603d, 604d, 605d, 606d, 607d, 608d, 609d, 610d, 611d, 612d, 613d, 614d, 615d, 616d, 617d, 618d, 619d, 620d, 621d, 622d, 623d, 624d, 625d, 626d, 627d, 628d, 629d, 630d, 631d, 632d, 633d, 634d, 635d, 636d, 637d, 638d, 639d, 640d, 641d, 642d, 643d, 644d, 645d, 646d, 647d, 648d, 649d, 650d, 651d, 652d, 653d, 654d, 655d, 656d, 657d, 658d, 659d, 660d, 661d, 662d, 663d, 664d, 665d, 666d, 667d, 668d, 669d, 670d, 671d, 672d, 673d, 674d, 675d, 676d, 677d, 678d, 679d, 680d, 681d, 682d, 683d, 684d, 685d, 686d, 687d, 688d, 689d, 690d, 691d, 692d, 693d, 694d, 695d, 696d, 697d, 698d, 699d, 700d, 800d, ], 
@@ -353,12 +416,37 @@ THICKNESS_value2: [26d-6, 26d-6, ],
 PROPERTY_LIST: ["EFFICIENCY", "RINDEX", "KINDEX", "THICKNESS", ]
 }
 
+{
+name: "OPTICS",
+index: "photocathode_R11780",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+photocathode: 1,
+
+EFFICIENCY_option: "wavelength",
+EFFICIENCY_value1: [60d, 200d, 260d, 270d, 280d, 285d, 290d, 300d, 310d, 330d, 370d, 420d, 475d, 500d, 530d, 570d, 600d, 630d, 670d, 700d, 800d, ],
+EFFICIENCY_value2: [0.0d, 0.0d, 0.0d, 0.0078d, 0.039d, 0.078d, 0.117d, 0.1404d, 0.156d, 0.23d, 0.3200d, 0.295d, 0.206d, 0.1656d, 0.1150d, 0.047d, 0.0195d, 0.0078d, 0.00078d, 0.0d, 0.0d],
+
+RINDEX_option: "wavelength",
+RINDEX_value1: [60d, 200d, 380d, 395d, 410d, 425d, 440d, 455d, 470d, 485d, 500d, 515d, 530d, 545d, 560d, 575d, 590d, 605d, 620d, 635d, 650d, 665d, 680d, 800d, ],
+RINDEX_value2: [1.d, 1.d, 1.92d, 2.18d, 2.38d, 2.61d, 2.70d, 2.87d, 3.00d, 3.00d, 3.00d, 3.09d, 3.26d, 3.20d, 3.12d, 3.06d, 3.01d, 2.98d, 2.96d, 2.95d, 2.95d, 2.95d, 2.96d, 3.d, ],
+
+KINDEX_option: "wavelength",
+KINDEX_value1: [60d, 200d, 380d, 395d, 410d, 425d, 440d, 455d, 470d, 485d, 500d, 515d, 530d, 545d, 560d, 575d, 590d, 605d, 620d, 635d, 650d, 665d, 680d, 800d, ],
+KINDEX_value2: [3.d, 3.d, 1.69d, 1.69d, 1.71d, 1.53d, 1.50d, 1.44d, 1.34d, 1.11d, 1.06d, 1.05d, 0.86d, 0.63d, 0.53d, 0.46d, 0.42d, 0.38d, 0.37d, 0.35d, 0.34d, 0.34d, 0.33d, 0.1d, ],
+
+THICKNESS_value1: [188d, 0d, ],
+THICKNESS_value2: [26d-6, 26d-6, ],
+PROPERTY_LIST: ["EFFICIENCY", "RINDEX", "KINDEX", "THICKNESS", ]
+}
+
 //// Below is the photocathode material for the ET-9390B.
 {
 name: "OPTICS",
 index: "photocathode_et9390b",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+photocathode: 1,
 
 EFFICIENCY_option: "wavelength", 
 EFFICIENCY_value1: [60.0d, 300.00d,305.00d,310.00d,315.00d,320.00d,325.00d,330.00d,335.00d,340.00d,345.00d,350.00d,355.00d,360.00d,365.00d,370.00d,375.00d,380.00d,385.00d,
@@ -531,6 +619,11 @@ name: "OPTICS",
 index: "acrylic_white",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.01,
 RINDEX_option: "wavelength",
 RINDEX_value1: [60d, 200d, 210d, 220d, 230d, 240d, 250d, 260d, 270d, 280d, 290d, 300d, 310d, 320d, 330d, 340d, 350d, 360d, 370d, 380d, 390d, 400d, 410d, 420d, 430d, 440d, 450d, 460d, 470d, 480d, 490d, 500d, 510d, 520d, 530d, 540d, 550d, 560d, 570d, 580d, 590d, 600d, 610d, 620d, 630d, 640d, 650d, 660d, 670d, 680d, 690d, 700d, 710d, 720d, 730d, 740d, 750d, 760d, 770d, 780d, 790d, 800d, ],
 RINDEX_value2: [1.597d, 1.597d, 1.584d, 1.573d, 1.564d, 1.556d, 1.550d, 1.544d, 1.539d, 1.534d, 1.531d, 1.527d, 1.524d, 1.521d, 1.519d, 1.516d, 1.514d, 1.512d, 1.510d, 1.509d, 1.507d, 1.506d, 1.505d, 1.503d, 1.502d, 1.501d, 1.500d, 1.499d, 1.499d, 1.498d, 1.497d, 1.496d, 1.496d, 1.495d, 1.494d, 1.494d, 1.493d, 1.493d, 1.492d, 1.492d, 1.491d, 1.491d, 1.490d, 1.490d, 1.490d, 1.489d, 1.489d, 1.488d, 1.488d, 1.488d, 1.488d, 1.487d, 1.487d, 1.487d, 1.486d, 1.486d, 1.486d, 1.486d, 1.485d, 1.485d, 1.485d, 1.485d, ],
@@ -706,6 +799,11 @@ name: "OPTICS",
 index: "tyvek",
 valid_begin : [0, 0],
 valid_end : [0, 0],
+surface: 1,
+finish: "ground",
+model: "glisur",
+type: "dielectric_metal",
+polish: 0.01,
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1:[60d, 800d, ],
 REFLECTIVITY_value2:[0.95d, 0.95d, ],
@@ -720,7 +818,9 @@ name: "OPTICS",
 index: "styrofoam",
 valid_begin : [0, 0],
 valid_end : [0, 0],
-
+surface: 1,
+finish: "ground",
+type: "dielectric_metal",
 //arbitrarily extended properties down to 60nm
 REFLECTIVITY_option: "wavelength",
 REFLECTIVITY_value1: [60d, 800d, ],

data/PMT.ratdb
@@ -62,8 +62,30 @@ wall_thickness: 3.0, // mm
 z_edge:   [ 4.020d,  		1.00d,   0.00d, -16.75d,   -30.00d, -32.00d, -119.0d], // rough guess. Photocathode details are wrong
 rho_edge: [ 38.0d ,	 	       38.0d, 38.0d,  38.0d,     26.65d, 26.65d, 26.65d],   // this assumes that the photocathode covers the entire front face of the PMT
 z_origin: [ -1000000.00d,  	0.00d,   0.00d,  10000.00d,  31.00d, 0.00d],      // and wraps back another 1mm, in reality the photocathode diameter is ~64mm 
-
 }
+
+{
+name: "PMT",
+index: "r11780_hqe",
+valid_begin: [0,0],
+valid_end: [0,0],
+
+dynode_material: "stainless_steel",
+glass_material: "glass",
+pmt_vacuum_material: "pmt_vacuum",
+photocathode_surface: "photocathode_R11780",
+mirror_surface: "mirror",
+dynode_surface: "stainless_steel",
+
+dynode_radius: 41.5, // mm
+dynode_top: -46.0, // mm
+wall_thickness: 3.0, // mm
+
+z_edge:   [112.50d, 79.59d, 0.75d, 0.00d, -79.59d, -119.79d, -127.50d, -285.0d],
+rho_edge: [0.00d, 108.87d, 151.35d, 151.50d, 108.87d, 66.48d, 63.0d, 63.0d],
+z_origin: [-84.00d, 0.00d, 0.00d, 0.00d, 84.00d, -127.5d, -285.0d],
+}
+
 {
 name: "PMT",
 index: "et9390b",

data/PMTTRANSIT.ratdb
@@ -1,0 +1,11 @@
+// Default is for zero cable delay, zero transit time
+// TODO: Index this by PMT type
+{
+name: "PMTTRANSIT",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+cable_delay: 0.0d,
+x: [ -0.001, 0.0, 0.001, ],
+y: [ 0.0d, 1.0d, 0.0d, ],
+}
+

data/sphere/PMTTRANSIT.ratdb
@@ -1,0 +1,10 @@
+// This is from the R11780 HQE
+{
+name: "PMTTRANSIT",
+valid_begin : [0, 0],
+valid_end : [0, 0],
+cable_delay: 0.0d,
+x: [ -24.5196 , -23.5188 , -22.518 , -21.5172 , -20.5164 , -19.5156 , -18.5148 , -17.514 , -16.5132 , -15.5124 , -14.5116 , -13.5108 , -12.51 , -11.5092 , -10.5084 , -9.5076 , -8.5068 , -7.506 , -6.5052 , -5.5044 , -4.5036 , -3.5028 , -2.502 , -1.5012 , -0.5004 , 0.5004 , 1.5012 , 2.502 , 3.5028 , 4.5036 , 5.5044 , 6.5052 , 7.506 , 8.5068 , 9.5076 , 10.5084 , 11.5092 , 12.51 , 13.5108 , 14.5116 , 15.5124 , 16.5132 , 17.514 , 18.5148 , 19.5156 , 20.5164 , 21.5172 , 22.518 , 23.5188 , 24.5196 , 25.5204 , 26.5212 , 27.522 , 28.5228 , 29.5236 , 30.5244 , 31.5252 , 32.526 , 33.5268 , 34.5276 , 35.5284 , 36.5292 , 37.53 , 38.5308 , 39.5316 , 40.5324 , 41.5332 , 42.534 , 43.5348 , 44.5356 , 45.5364 , 46.5372 , 47.538 , 48.5388 , 49.5396 , 50.5404 , 51.5412 , 52.542 , 53.5428 , 54.5436 , 55.5444 , 56.5452 , 57.546 , 58.5468 , 59.5476 , 60.5484 , 61.5492 , 62.55 , 63.5508 , 64.5516 ] ,
+y: [ 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0d , 0.00134046 , 0.00409797 , 0.0136918 , 0.0482947 , 0.149978 , 0.377568 , 0.670669 , 0.838628 , 0.894295 , 0.913406 , 0.922483 , 0.928094 , 0.931024 , 0.933609 , 0.935237 , 0.936673 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.937956 , 0.939143 , 0.940331 , 0.941709 , 0.942992 , 0.944792 , 0.94619 , 0.947492 , 0.948795 , 0.950365 , 0.951954 , 0.953524 , 0.954922 , 0.956722 , 0.958656 , 0.960208 , 0.962122 , 0.96375 , 0.965416 , 0.966833 , 0.968423 , 0.970395 , 0.97208 , 0.973516 , 0.975508 , 0.977346 , 0.979146 , 0.981329 , 0.983589 , 0.986098 , 0.988549 , 0.991517 , 0.995021 , 0.998085 , 1.0d ]
+}
+

data/sphere/sphere.geo
@@ -25,23 +25,10 @@ color: [0.4, 0.4, 0.6, 0.05],
 
 {
 name: "GEO",
-index: "vessel",
+index: "target",
 valid_begin: [0, 0],
 valid_end: [0, 0],
 mother: "water",
-type: "sphere",
-r_max: 6030.0,
-position: [0.0, 0.0, 0.0],
-material: "acrylic_uvt",
-color: [0.4, 0.4, 0.6, 0.1],
-}
-
-{
-name: "GEO",
-index: "inner",
-valid_begin: [0, 0],
-valid_end: [0, 0],
-mother: "vessel",
 type: "sphere",
 r_max: 6000.0,
 position: [0.0, 0.0, 0.0],
@@ -56,10 +43,9 @@ valid_begin: [0, 0],
 valid_end: [0, 0], 
 mother: "water", 
 type: "pmtarray", 
-pmt_type: "r1408", 
+pmt_type: "r11780_hqe", 
 pmt_detector_type: "idpmt",
 sensitive_detector: "/mydet/pmt/inner", 
-efficiency_correction: 1.027,  
 pos_table: "PMTINFO", 
 orientation: "point",
 orient_point: [0.0, 0.0, 0.0], 

commit cf733e8f2d6c53da9a9a863cdc33025e7f11c367

make DS::MCPMT getter naming consistent
* Change from e.g. GetPMT to GetMCPMT. Getter names generally
  use the class name.

* Also fix a bug introduced in ea080d12b28b5f4b8113d7aaf2cb6017134badbd,
  where using `DBL_MAX` instead of a large double caused segfaults.
  The underlying reason for this apparently nonsense result is
  under investigation.

src/core/Gsim.cc
@@ -473,8 +473,8 @@ void Gsim::MakeEvent(const G4Event* g4ev, DS::Root* ds) {
   GLG4HitPMTCollection* hitpmts = GLG4VEventAction::GetTheHitPMTCollection();
   int numPE = 0;
  
-  double firsthittime = DBL_MAX;
-  double lasthittime = -DBL_MAX;
+  double firsthittime = 99999;
+  double lasthittime = -99999;
 
   // Get the PMT type for IDPMTs. Then in the loop,
   // increment numPE only when the PE is in an IDPMT.
@@ -487,7 +487,7 @@ void Gsim::MakeEvent(const G4Event* g4ev, DS::Root* ds) {
 
     // Create and initialize a RAT DS::MCPMT 
     // note that GLG4HitPMTs are given IDs which are their index
-    DS::MCPMT* rat_mcpmt = mc->AddNewPMT();
+    DS::MCPMT* rat_mcpmt = mc->AddNewMCPMT();
     mcpmtObjects[a_pmt->GetID()] = rat_mcpmt;
     rat_mcpmt->SetID(a_pmt->GetID());
     DS::Run* run = DS::RunStore::GetRun(runID);
@@ -544,7 +544,7 @@ void Gsim::MakeEvent(const G4Event* g4ev, DS::Root* ds) {
     //hit->SetIsNoise();
     // Add the PMT if it did not register a "real" hit
     if (!mcpmtObjects.count(pmtid)) {
-      DS::MCPMT* rat_mcpmt = mc->AddNewPMT();
+      DS::MCPMT* rat_mcpmt = mc->AddNewMCPMT();
       mcpmtObjects[pmtid] = rat_mcpmt;
       rat_mcpmt->SetID(pmtid);
       rat_mcpmt->SetType(pmtinfo->GetType(pmtid));

src/core/PruneProc.cc
@@ -88,8 +88,8 @@ Processor::Result PruneProc::DSEvent(DS::Root *ds) {
       mc->PrunePMT();
 
     if (mc_pmt_photon) {
-      for (int i=0; i < mc->GetPMTCount(); i++)
-        mc->GetPMT(i)->PruneMCPhoton();
+      for (int i=0; i < mc->GetMCPMTCount(); i++)
+        mc->GetMCPMT(i)->PruneMCPhoton();
     }
   }
   

src/daq/SimpleDAQProc.cc
@@ -32,8 +32,8 @@ Processor::Result SimpleDAQProc::DSEvent(DS::Root *ds) {
 
   double totalQ = 0.0;
   double calibQ = 0.0;
-  for (int imcpmt=0; imcpmt < mc->GetPMTCount(); imcpmt++) {
-      DS::MCPMT *mcpmt = mc->GetPMT(imcpmt);
+  for (int imcpmt=0; imcpmt < mc->GetMCPMTCount(); imcpmt++) {
+      DS::MCPMT *mcpmt = mc->GetMCPMT(imcpmt);
       int pmtID = mcpmt->GetID();
 
       if (mcpmt->GetMCPhotonCount() > 0) {

src/ds/MC.hh
@@ -85,9 +85,9 @@ public:
   }
 
   /** List of PMTs which had at least one photoelectron generated */
-  virtual MCPMT* GetPMT(int i) { return &pmt[i]; }
-  virtual int GetPMTCount() const { return pmt.size(); }
-  virtual MCPMT* AddNewPMT() {
+  virtual MCPMT* GetMCPMT(int i) { return &pmt[i]; }
+  virtual int GetMCPMTCount() const { return pmt.size(); }
+  virtual MCPMT* AddNewMCPMT() {
     pmt.resize(pmt.size() + 1);
     return &pmt.back();
   };