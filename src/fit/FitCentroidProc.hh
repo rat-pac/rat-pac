bf3f09f96708d9af61b24e75d945c6df4a293f9c
Andy Mastbaum <amastbaum@gmail.com>
2014-07-09 15:04:15 -0400

    fix the centroid fitter

--- src/fit/FitCentroidProc.hh
+++ src/fit/FitCentroidProc.hh
@@ -1,24 +1,33 @@
 #ifndef __RAT_FitCentroidProc__
 #define __RAT_FitCentroidProc__
 
+#include <string>
 #include <RAT/Processor.hh>
 
 namespace RAT {
+
+namespace DS {
+  class Root;
+  class EV;
+}
   
 class FitCentroidProc : public Processor {
 public:
   FitCentroidProc();
-  virtual ~FitCentroidProc() { };
+  virtual ~FitCentroidProc() {}
   
   /** param = "power", value = exponent to raise charge to when averaging
    *  default is 2.0 */
   virtual void SetD(std::string param, double value);
 
-  virtual Processor::Result Event(DS::Root *ds, DS::EV *ev);
+  virtual Processor::Result Event(DS::Root* ds, DS::EV* ev);
+
 protected:
   double fPower;
   double fRescale;
 };
 
 } // namespace RAT
-#endif
+
+#endif  // __RAT_FitCentroidProc__
+
