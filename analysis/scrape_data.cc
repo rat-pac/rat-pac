fa0d4474977ac67bc2f53c67d3bdac4b6c402dab
Taritree M Wongjirad <taritree@hibat0003.cmsaf.mit.edu>
2015-04-03 16:58:05 -0400

    added new variables to scrape_data

--- analysis/scrape_data.cc
+++ analysis/scrape_data.cc
@@ -11,7 +11,11 @@ using namespace std;
 
 int main( int nargs, char** argv ) {
 
-  std::string inputfile = "../kpipeout_test.root";
+  double prompt_cut = 500.0; // ns
+
+  //std::string inputfile = "../kpipeout_test.root";
+  //std::string inputfile = "/net/nudsk0001/d00/scratch/taritree/kpipe_out/first_set/run1_kpipe_output.root";
+  std::string inputfile = "/net/nudsk0001/d00/scratch/taritree/kpipe_out/run100_kpipe.root";
   DSReader* ds = new DSReader( inputfile.c_str() ); 
 
   TFile* tf_pmtinfo = new TFile( "../data/kpipe/PMTINFO.root", "open" );
@@ -28,8 +32,12 @@ int main( int nargs, char** argv ) {
   int npe_late;
   int npmts;
   int nhoops;
-  double pos[3];  // recon position
-  double r,z;     // recon
+  double pos[3];
+  double r,z;
+  double pos_prompt[3];  // recon position
+  double r_prompt,z_prompt;     // recon
+  double pos_late[3];  // recon position
+  double r_late,z_late;     // recon
   double qhpos[3]; // recon using quarter hoop
   double qhr,qhz;  // recon using quarter hoop
   double posv[3]; // true vertex
@@ -42,12 +50,18 @@ int main( int nargs, char** argv ) {
   TTree* tree = new TTree( "mcdata", "MC Data" );
   // recon
   tree->Branch( "npe", &npe, "npe/I" );
-  tree->Branch( "npe_prompt", &npe_prompt, "npe_late/I" );
+  tree->Branch( "npe_prompt", &npe_prompt, "npe_prompt/I" );
   tree->Branch( "npe_late", &npe_late, "npe_late/I" );
   tree->Branch( "npmts", &npmts, "npmts/I" );
   tree->Branch( "pos", pos, "pos[3]/D" );
   tree->Branch( "r", &r, "r/D" );
   tree->Branch( "z", &z, "z/D" );
+  tree->Branch( "pos_prompt", pos_prompt, "pos_prompt[3]/D" );
+  tree->Branch( "r_prompt", &r_prompt, "r_prompt/D" );
+  tree->Branch( "z_prompt", &z_prompt, "z_prompt/D" );
+  tree->Branch( "pos_late", pos_late, "pos_late[3]/D" );
+  tree->Branch( "r_late", &r_late, "r_late/D" );
+  tree->Branch( "z_late", &z_late, "z_late/D" );
   tree->Branch( "qhpos", qhpos, "qhpos[3]/D" );
   tree->Branch( "qhr", &qhr, "qhr/D" );
   tree->Branch( "qhz", &qhz, "qhz/D" );
@@ -64,22 +78,25 @@ int main( int nargs, char** argv ) {
 
   long ievent = 0;
   long nevents = ds->GetTotal();
-
+  nevents = 90000;
   
   while (ievent<nevents) {
     DS::Root* root = ds->NextEvent();
 
-    if ( ievent%1==0 )
+    if ( ievent%1000==0 )
       std::cout << "Event " << ievent << std::endl;
 
     DS::MC* mc = root->GetMC();
     npe = mc->GetNumPE();
     npmts = mc->GetMCPMTCount();
 
+    if ( mc->GetMCParticleCount()==0 )
+      continue;
+
     // true vertex
-    posv[0] = mc->GetMCParticle(0)->GetPosition().X();
-    posv[1] = mc->GetMCParticle(0)->GetPosition().Y();
-    posv[2] = mc->GetMCParticle(0)->GetPosition().Z();
+    posv[0] = mc->GetMCParticle(0)->GetPosition().X()/10.0; //change to cm
+    posv[1] = mc->GetMCParticle(0)->GetPosition().Y()/10.0; //change to cm
+    posv[2] = mc->GetMCParticle(0)->GetPosition().Z()/10.0; //change to cm
     rv = sqrt(posv[0]*posv[0] + posv[1]*posv[1]);
     zv = posv[2];
 
@@ -103,7 +120,10 @@ int main( int nargs, char** argv ) {
     npe_prompt = npe_late = 0;
     tstart = tave = 0.0;
     r = z = qhr = qhz = 0.0;
+    r_prompt = z_prompt = r_late = z_late = 0.0;
     pos[0] = pos[1] = pos[2] = 0.0;
+    pos_late[0] = pos_late[1] = pos_late[2] = 0.0;
+    pos_prompt[0] = pos_prompt[1] = pos_prompt[2] = 0.0;
     qhpos[0] = qhpos[1] = qhpos[2] = 0.0;
     tave = tstart = tave_dcye = tstart_dcye = 0.0;
     
@@ -154,19 +174,24 @@ int main( int nargs, char** argv ) {
 	  DS::MCPhoton* hit = pmt->GetMCPhoton(ihit);
 	  // earliest time
 	  double t = hit->GetHitTime();
-	  if ( t<1000.0 ) {
+	  if ( t<prompt_cut ) {
 	    npe_prompt += 1;
 	    tave += t;
 	    if ( tstart>t ) 
 	      tstart = t;
+	    for (int v=0; v<3; v++)
+	      pos_prompt[v] += pmtpos[v];
 	  }
 	  else {
 	    npe_late += 1;
 	    tave_dcye += t;
 	    if ( tstart_dcye>t )
 	      tstart_dcye = t;
+	    for (int v=0; v<3; v++)
+	      pos_late[v] += pmtpos[v];
+
 	  }
-	}
+	}//end of hit loop
       
       }//end of hit PMT loop
 
@@ -174,6 +199,8 @@ int main( int nargs, char** argv ) {
       for (int v=0; v<3; v++) {
 	pos[v] /= double(npe);
 	qhpos[v] /= double(npe);
+	pos_prompt[v] /= double(npe_prompt);
+	pos_late[v] /= double(npe_late);
       }
       tave /= double(npe_prompt);
       tave_dcye /= double(npe_late);
@@ -181,11 +208,15 @@ int main( int nargs, char** argv ) {
       // r,z
       r = sqrt(pos[0]*pos[0]+pos[1]*pos[1]);
       z = pos[2];
+      r_prompt = sqrt(pos_prompt[0]*pos_prompt[0]+pos_prompt[1]*pos_prompt[1]);
+      z_prompt = pos_prompt[2];
+      r_late = sqrt(pos_late[0]*pos_late[0]+pos_late[1]*pos_late[1]);
+      z_late = pos_late[2];
       qhr = sqrt(qhpos[0]*qhpos[0]+qhpos[1]*qhpos[1]);
       qhz = qhpos[2];
 
       nhoops = 0;
-      for (int i=0; i<10; i++)
+      for (int i=0; i<1000; i++)
 	nhoops += hoop_hit[i];
     }//end of if pe
     ievent++;
