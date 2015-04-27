fe87203556ed403a4fb0dd98df028fa1b138c1e9
DoubleChooz Service Account dchooz <dchooz@t2srv0008.cmsaf.mit.edu>
2015-04-26 22:41:47 -0400

    turn on hadronic processes

--- tier2scripts/gen_macro.py
+++ tier2scripts/gen_macro.py
@@ -2,7 +2,7 @@ import os,sys
 
 def gen_macro( outfile, jobid, nevents_per_job ):
     part1 = """/glg4debug/glg4param omit_muon_processes  0.0
-/glg4debug/glg4param omit_hadronic_processes  1.0
+/glg4debug/glg4param omit_hadronic_processes  0.0
 
 /rat/db/set DETECTOR experiment \"kpipe\"
 /rat/db/set DETECTOR geo_file \"kpipe/kpipe_wsipms.geo\"
