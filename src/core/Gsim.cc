499a1f4bfcba2635c54946fda2a171b4798b4661
pershint <tjpershing@ucdavis.edu>
2015-05-29 15:53:25 -0700

    Added the ReacIBDgen definitions necessary in Gsim.cc

--- src/core/Gsim.cc
+++ src/core/Gsim.cc
@@ -12,12 +12,14 @@
 #include <RAT/Factory.hh>
 #include <RAT/GLG4VertexGen.hh>
 #include <RAT/VertexGen_IBD.hh>
+#include <RAT/VertexGen_ReacIBD.hh>
 #include <RAT/Gen_LED.hh>
 #include <RAT/VertexGen_ES.hh>
 #include <RAT/VertexGen_Spectrum.hh>
 #include <RAT/DecayChain_Gen.hh>
 #include <RAT/Coincidence_Gen.hh>
 #include <RAT/CfGen.hh>
+#include <RAT/ReacIBDgen.hh>
 #include <RAT/EventInfo.hh>
 #include <RAT/TrackInfo.hh>
 
@@ -92,7 +94,10 @@ void Gsim::Init() {
   GlobalFactory<GLG4VertexGen>::Register("ibd",
                                          new Alloc<GLG4VertexGen,
                                          VertexGen_IBD>);
-  GlobalFactory<GLG4VertexGen>::Register("es",
+  GlobalFactory<GLG4VertexGen>::Register("ibd",
+                                         new Alloc<GLG4VertexGen,
+                                         VertexGen_ReacIBD>);
+   GlobalFactory<GLG4VertexGen>::Register("es",
                                          new Alloc<GLG4VertexGen,
                                          VertexGen_ES>);
   GlobalFactory<GLG4VertexGen>::Register("spectrum",
